a24e63e7e49447073d66ef0aedde2bd8747d69ef
shameRRR <118363645+shameRRR@users.noreply.github.com>
2022-11-17 18:17:02 +0300

    Add files via upload

---
+++ задача 1 Перминов Артём, Колпащиков Роман.pas
@@ -0,0 +1,20 @@
+﻿program lol;
+const n=3;
+var a:array[1..n,1..n]of integer;
+    i,j,max,min:integer;
+begin
+for i:=1 to n do
+for j:=1 to n do
+read(a[i,j]);
+max:=a[1,1];
+for i:=1 to n do
+for j:=2 to n do
+if a[i,j]>max then max:=a[i,j];
+writeln('Максимальный элемент: ',max);
+min:=a[1,1];
+for i:=1 to n do
+for j:=2 to n do
+if a[i,j]<min then min:=a[i,j];
+writeln('Минимальный элемент: ',min);
+writeln(max*min);
+end.
